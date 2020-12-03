e85d060a089a0459993ef819e4e3d524970070fd
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2020-12-03 11:45:03 +0200

    refactoring

--- wilma_schedules_reader/__init__.py
+++ wilma_schedules_reader/__init__.py
@@ -1,5 +1,5 @@
-"""wilma_schedules_reader - The rest client which reads shedules from the ."""
+"""wilma_schedules_reader - The rest client which reads shedules from Wilma service."""
 
-__version__ = '0.1.1'
-__author__ = 'Pasi Ollikainen <pasi.ollikainen@outlook.com>'
+__version__ = "0.1.1"
+__author__ = "Pasi Ollikainen <pasi.ollikainen@outlook.com>"
 __all__ = []
--- wilma_schedules_reader/reader.py
+++ wilma_schedules_reader/reader.py
@@ -27,9 +27,9 @@ def __get_session_key(url: str, s: requests.Session) -> str:
         sys.exit(1)
 
 
-def __fidate2pydate(date_fi: str, logger: logging.Logger) -> date:
+def __fidate2pydate(fi_date: str, logger: logging.Logger) -> date:
     try:
-        splitted_date = str(date_fi).split(".")
+        splitted_date = str(fi_date).split(".")
         return date(int(splitted_date[2]), int(splitted_date[1]), int(splitted_date[0]))
     except Exception as error:
         logger.error(f"Getting timedelta failed: {error}")
@@ -39,37 +39,40 @@ def __fidate2pydate(date_fi: str, logger: logging.Logger) -> date:
 def __get_time_delta(start: str, end: str, logger: logging.Logger) -> timedelta:
     s = __fidate2pydate(start, logger)
     e = __fidate2pydate(end, logger)
-    return s - e
+    return e - s
 
 
 def __get_dates(start: str, end: str, logger: logging.Logger) -> List[str]:
     dates = list()
     delta = __get_time_delta(start, end, logger)
+    print(f"{delta}")
     for d in range(delta.days + 1):
-        day = __fidate2pydate(start) + timedelta(days=d)
+        day = __fidate2pydate(start, logger) + timedelta(days=d)
         dates.append(str(day.strftime("%d.%m.%Y")))
     return dates
 
 
 def valid_type(type: str) -> bool:
-    if type in ["room", "teacher", "student"]:
+    if type in ["rooms", "teachers", "students"]:
         return True
     else:
         return False
 
 
 @click.command()
-@click.argument("resourceType", type=click.STRING)
-@click.argument("startDate", type=click.STRING)
-@click.argument("endDate", type=click.STRING)
-@click.argument("wilmaURL", type=click.STRING)
+@click.argument("resource_type", type=click.STRING)
+@click.argument("start_date", type=click.STRING)
+@click.argument("end_date", type=click.STRING)
+@click.argument("wilma_url", type=click.STRING)
 @click.argument("user", type=click.STRING)
 @click.argument("password", type=click.STRING)
 @click.argument("apikey", type=click.STRING)
-@click.argument("outputPath", type=click.Path(exists=True))
-def main(resourceType, startDate, endDate, wilmaURL, user, password, apikey):
-    if valid_type:
-        wilma_url = f"https://{wilmaURL}"
+@click.argument("output_path", type=click.Path(exists=True))
+def main(
+    resource_type, start_date, end_date, wilma_url, user, password, apikey, output_path
+):
+    if valid_type(resource_type):
+        wilma_url = f"https://{wilma_url}/"
         s = requests.Session()
         sessionid = __get_session_key(wilma_url, s)
         apikey = hashlib.sha1(
@@ -87,9 +90,10 @@ def main(resourceType, startDate, endDate, wilmaURL, user, password, apikey):
 
         if r.status_code == 200:
             logger.info(f"Logged succesfully in. Getting status code {r.status_code}.")
-            dates = __get_dates(start=startDate, end=endDate, logger=logger)
+            dates = __get_dates(start=start_date, end=end_date, logger=logger)
+            print(f"{dates}")
             for day in dates:
-                schedule = f"schedule/index_json?p={day}&f={day}&{resourceType}=all"
+                schedule = f"schedule/index_json?p={day}&f={day}&{resource_type}=all"
                 succeed = False
                 r = None
                 while not succeed:
@@ -98,13 +102,15 @@ def main(resourceType, startDate, endDate, wilmaURL, user, password, apikey):
                         succeed = True
                     except Exception as error:
                         logger.error(
-                            f"Failing to get url: {wilma_url + schedule}.\n{error}"
+                            f"Failing to get url: {wilma_url + schedule}: {error}"
                         )
                         logger.info("Sleeping 20 seconds.")
                         time.sleep(20)
                 try:
                     with open(
-                        f"data/{resourceType}-{day}-data.json", "w", encoding="utf-8"
+                        f"{output_path}/{resource_type}-{day}-data.json",
+                        "w",
+                        encoding="utf-8",
                     ) as output_file:
                         json.dump(r.json(), output_file)
                 except Exception as error:
@@ -113,13 +119,13 @@ def main(resourceType, startDate, endDate, wilmaURL, user, password, apikey):
                     )
                     sys.exit(1)
                 time.sleep(1)
-                logger.info(f"Processed resource {resourceType} at the date {day}.")
+                logger.info(f"Processed resource {resource_type} at the date {day}.")
         else:
             logger.critical(
                 f"Loging failed. Getting status code {r.status_code}. Nothing to do."
             )
     else:
-        logger.critical(f"Resource type {resourceType} is not valid.")
+        logger.critical(f"Resource type {resource_type} is not valid.")
 
 
 if __name__ == "__main__":
