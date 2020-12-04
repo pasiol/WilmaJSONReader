91847b160e215dc20a2a1fa7e835b91f2375d667
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2020-12-04 10:14:19 +0200

    Bump version: 0.2.0 → 0.2.1

--- wilma_schedules_reader/__init__.py
+++ wilma_schedules_reader/__init__.py
@@ -1,5 +1,5 @@
 """wilma_schedules_reader - The rest client which reads shedules from Wilma service."""
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Pasi Ollikainen <pasi.ollikainen@outlook.com>"
 __all__ = []
