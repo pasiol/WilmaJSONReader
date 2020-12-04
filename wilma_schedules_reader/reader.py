f6c0e14a3f3457147cbc959e1e574620042141ef
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2020-12-04 10:11:09 +0200

    refactored to the class object

--- wilma_schedules_reader/reader.py
+++ wilma_schedules_reader/reader.py
@@ -5,6 +5,7 @@ import requests
 import json
 import logging
 import click
+import validators
 import mypy
 from datetime import date, timedelta
 from typing import List
@@ -13,15 +14,29 @@ from typing import List
 class WilmaJSONReader:
 
     logger = None
+    wilma_url = None
+    session = requests.Session()
+    __session_api_key = None
+    __apikey = None
+    __user = None
+    __password = None
 
-    def __init__(self):
-        print("init")
+    def __init__(self, wilma_url: str, user: str, password: str, apikey: str):
+
         logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger(__name__)
+        self.wilma_url = f"https://{wilma_url}/"
+        if not validators.url(self.wilma_url):
+            self.logger.critical(f"Wilma URL {self.wilma_url} is not valid.")
+            sys.exit(1)
+        self.__user = user
+        self.__password = password
+        self.__apikey = apikey
+        self.logger.info(f"WilmaJSONReader object initialized succesfully.")
 
-    def get_session_key(self, url: str, s: requests.Session) -> str:
+    def get_session_key(self) -> str:
         try:
-            r = s.get(url + "index_json", verify=True)
+            r = self.session.get(self.wilma_url + "index_json", verify=True)
             json_response = json.loads(r.text)
             if "SessionID" in json_response.keys():
                 self.logger.info(f"Getting session key succesfully.")
@@ -30,6 +45,32 @@ class WilmaJSONReader:
                 return ""
         except Exception as error:
             self.logger.error(f"Getting session key failed: {error}")
+            sys.exit(1)
+
+    def login(self):
+        sessionid = self.get_session_key()
+        self.__session_api_key = hashlib.sha1(
+            str(f"{self.__user}|{sessionid}|{self.__apikey}").encode("utf-8")
+        ).hexdigest()
+        data = {
+            "Login": self.__user,
+            "Password": self.__password,
+            "SessionId": sessionid,
+            "ApiKey": "sha1:" + self.__session_api_key,
+            "format": "json",
+        }
+        headers = {"accept": "application/json"}
+        r = self.session.post(
+            self.wilma_url + "login", data=data, headers=headers, verify=True
+        )
+        if r.status_code == 200:
+            self.logger.info(
+                f"Logged succesfully in. Getting status code {r.status_code}."
+            )
+        else:
+            self.logger.info(
+                f"Loging failed, status code {r.status_code}. Nothing to do."
+            )
             sys.exit(1)
 
     def __fidate2pydate(self, fi_date: str, logger: logging.Logger) -> date:
@@ -52,17 +93,39 @@ class WilmaJSONReader:
     def get_dates(self, start: str, end: str, logger: logging.Logger) -> List[str]:
         dates = list()
         delta = self.__get_time_delta(start, end, logger)
-        print(f"{delta}")
         for d in range(delta.days + 1):
             day = self.__fidate2pydate(start, logger) + timedelta(days=d)
             dates.append(str(day.strftime("%d.%m.%Y")))
         return dates
 
-    def valid_type(self, type: str) -> bool:
+    def validate_schedule_type(self, type: str) -> bool:
         if type in ["rooms", "teachers", "students"]:
             return True
         else:
             return False
+
+    def get_schedule(self, day: str, resource_type: str) -> requests.models.Response:
+        schedule = None
+        try:
+            schedule = f"schedule/index_json?p={day}&f={day}&{resource_type}=all"
+            r = self.session.get(self.wilma_url + schedule)
+            return r
+        except Exception as error:
+            self.logger.error(
+                f"Failing to get url: {self.wilma_url + schedule}: {error}"
+            )
+            return None
+
+
+def write_json_file(
+    filename: str, r: requests.models.Response, logger: logging.Logger
+) -> None:
+    try:
+        with open(filename, "w", encoding="utf-8") as output_file:
+            json.dump(r.json(), output_file)
+    except Exception as error:
+        logger.critical(f"Writing output file failed: {error}. Nothing to do.")
+        sys.exit(1)
 
 
 @click.command()
@@ -85,66 +148,28 @@ def main(
     output_path,
 ):
 
-    reader = WilmaJSONReader()
-    if reader.valid_type(resource_type):
-        wilma_url = f"https://{wilma_url}/"
-        s = requests.Session()
-        sessionid = reader.get_session_key(wilma_url, s)
-        apikey = hashlib.sha1(
-            str(f"{user}|{sessionid}|{apikey}").encode("utf-8")
-        ).hexdigest()
-        data = {
-            "Login": user,
-            "Password": password,
-            "SessionId": sessionid,
-            "ApiKey": "sha1:" + apikey,
-            "format": "json",
-        }
-        headers = {"accept": "application/json"}
-        r = s.post(wilma_url + "login", data=data, headers=headers, verify=True)
-
-        if r.status_code == 200:
-            reader.logger.info(
-                f"Logged succesfully in. Getting status code {r.status_code}."
-            )
-            dates = reader.get_dates(
-                start=start_date, end=end_date, logger=reader.logger
-            )
-            print(f"{dates}")
-            for day in dates:
-                schedule = f"schedule/index_json?p={day}&f={day}&{resource_type}=all"
-                succeed = False
-                r = None
-                while not succeed:
-                    try:
-                        r = s.get(wilma_url + schedule)
-                        succeed = True
-                    except Exception as error:
-                        reader.logger.error(
-                            f"Failing to get url: {wilma_url + schedule}: {error}"
-                        )
-                        reader.logger.info("Sleeping 20 seconds.")
-                        time.sleep(20)
-                try:
-                    with open(
+    reader = WilmaJSONReader(wilma_url, user, password, apikey)
+    reader.login()
+    if reader.validate_schedule_type(resource_type):
+        dates = reader.get_dates(start=start_date, end=end_date, logger=reader.logger)
+        for day in dates:
+            succeed = False
+            while not succeed:
+                r = reader.get_schedule(day, resource_type)
+                if r.status_code == 200:
+                    succeed = True
+                    write_json_file(
                         f"{output_path}/{resource_type}-{day}-data.json",
-                        "w",
-                        encoding="utf-8",
-                    ) as output_file:
-                        json.dump(r.json(), output_file)
-                except Exception as error:
-                    reader.logger.critical(
-                        f"Writing output file failed: {error}. Nothing to do."
+                        r,
+                        reader.logger,
                     )
-                    sys.exit(1)
-                time.sleep(1)
-                reader.logger.info(
-                    f"Processed resource {resource_type} at the date {day}."
-                )
-        else:
-            reader.logger.critical(
-                f"Loging failed. Getting status code {r.status_code}. Nothing to do."
-            )
+                else:
+                    reader.logger.info(
+                        f"Getting status code: {r.status_code} Sleeping 20 seconds and trying again."
+                    )
+                    time.sleep(20)
+            time.sleep(1)
+            reader.logger.info(f"Processed resource {resource_type} at the date {day}.")
     else:
         reader.logger.critical(f"Resource type {resource_type} is not valid.")
 
